4b01ab14b114aac4a3011adbfe340a4389c93dbe
Aleksandra Zhuravleva <aleksandra.zhuravleva@aalto.fi>
2019-01-22 23:40:38 +0200

    Add simple view and template to see list of uploaded games

--- app/gamestore/views/game.py
+++ app/gamestore/views/game.py
@@ -1,21 +1,22 @@
 from django.shortcuts import render, redirect
-from gamestore.models import UserProfile
+from gamestore.models import UserProfile, Game
 from gamestore.forms import GameForm
 from django.contrib.auth.decorators import login_required
 
 @login_required(login_url='/login/')
 def show_uploaded_games(request):
     user = UserProfile.objects.get(user=request.user)
-    # UNCOMMENT LATER!
+    # CHECK LATER !!!!
     #if not user.is_developer():
     #    return redirect('index')
+    games = Game.objects.filter(developer=user)
 
-    return render(request, 'game/uploaded_games.html', {})
+    return render(request, 'game/uploaded_games.html', {'games': games})
     
 @login_required(login_url='/login/')
 def add_game(request):
     user = UserProfile.objects.get(user=request.user)
-    # UNCOMMENT LATER!
+    # CHECK LATER !!!!
     #if not user.is_developer():
     #    return redirect('index')
     if request.method == 'POST':
@@ -31,6 +32,9 @@ def add_game(request):
             return render(request, 'game/add_game.html', {'form': form})
         else:
             #adding success, needs redirect
-            return redirect('index') # CHANGE REDIRECTED PAGE LATER!
+            return redirect('uploaded_games') # CHANGE REDIRECTED PAGE LATER!
     form = GameForm()
     return render(request, 'game/add_game.html', {'form': form})
+
+
+    
