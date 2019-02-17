9c8ade59353d11ac7749cedb48ffda1769f24eff
Aleksandra Zhuravleva <aleksandra.zhuravleva@aalto.fi>
2019-02-17 06:37:45 +0200

    Make allowed hosts [] for debug true

--- wsd/settings.py
+++ wsd/settings.py
@@ -26,10 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    '127.0.0.1',
-    'localhost'
-]
+ALLOWED_HOSTS = []
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
