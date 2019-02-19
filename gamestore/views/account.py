df262e09c1b67567ecfb12663d7462152997dee7
Aleksandra Zhuravleva <aleksandra.zhuravleva@aalto.fi>
2019-02-19 05:07:38 +0200

    Add import for httprequest

--- gamestore/views/account.py
+++ gamestore/views/account.py
@@ -33,7 +33,6 @@ from django.db import transaction
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404
 from gamestore.constants import *
-
 from django.contrib.sites.shortcuts import get_current_site
 from django.template.loader import render_to_string
 from ..tokens import account_activation_token
@@ -42,6 +41,7 @@ from django.utils.encoding import force_bytes, force_text
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.core.exceptions import ObjectDoesNotExist
 import datetime
+from django.http import HttpRequest
 
 def startpage(request):
     return render(request, BASE_HTML)
