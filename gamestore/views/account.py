d559f7364e34abfee04f128738ab0fca4376389d
Aleksandra Zhuravleva <aleksandra.zhuravleva@aalto.fi>
2019-02-19 05:44:10 +0200

    post_request

--- gamestore/views/account.py
+++ gamestore/views/account.py
@@ -65,7 +65,7 @@ def save_profile(backend, user, response, *args, **kwargs):
         #request = HttpRequest()
         rf = RequestFactory()
         post_request = rf.post('/login/', {'username': user_object.username})
-        auth_login(request, user_auth)
+        auth_login(post_request, user_auth)
     if profile is None:
         pass
     return render(None, BASE_HTML, {'profile': backend, 'resp' : resp, 'args':args})
