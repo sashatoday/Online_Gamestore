b50a09b442b6fea61cc00afd21a01b901c31a743
Aleksandra Zhuravleva <aleksandra.zhuravleva@aalto.fi>
2019-02-19 17:57:44 +0200

    Trying to fix usual password

--- gamestore/auth_backend.py
+++ gamestore/auth_backend.py
@@ -1,11 +1,22 @@
 from django.contrib.auth.backends import ModelBackend
 from django.contrib.auth.models import User
+from django.contrib.auth.hashers import check_password
 
 
 class PasswordlessAuthBackend(ModelBackend):
     """Log in to Django without providing a password."""
-    def authenticate(self, username=None):
-        try:
-            return User.objects.get(username=username)
-        except User.DoesNotExist:
-            return None
+    def authenticate(self, username=None, password=None):
+        if password:
+            try:
+                user = User.objects.get(username=username)
+                if (check_password(password, user.password)):
+                    return user
+                else:
+                    return None
+            except User.DoesNotExist:
+                return None
+        else:
+            try:
+                return User.objects.get(username=username)
+            except User.DoesNotExist:
+                return None
