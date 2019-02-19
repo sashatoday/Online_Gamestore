a184d33248835d62ca38f9c15147a8c5a4e2ed9f
Aleksandra Zhuravleva <aleksandra.zhuravleva@aalto.fi>
2019-02-19 18:55:43 +0200

    Fix ! to not

--- gamestore/views/account.py
+++ gamestore/views/account.py
@@ -50,7 +50,7 @@ def startpage(request):
 def save_facebook_profile(backend, user, response, *args, **kwargs):
     if backend.name == 'facebook':
         request = kwargs.get('request', None)
-        if ! response['email']:
+        if not response['email']:
             message = "Sorry, Facebook didn't provide your email. Please try again or sign up manually"
             return render(request, ERROR_HTML, {'message': message})
         if user:
